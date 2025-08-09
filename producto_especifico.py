a018e952a590e6b21e189debfaf270daf27244f8
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-08-08 21:50:01 -0400

    version 2

--- producto_especifico.py
+++ producto_especifico.py
@@ -1,11 +1,16 @@
 def producto_especifico(ruta_archivo,producto):
     try:
+        #abre el archivo
         with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            #lee el archivo y lo guarda en una lista
             lineas = archivo.readlines()
+            #recorre la lista
             for linea in lineas:
+                #busca el producto en la linea
                 if producto in linea:
                     print(linea)
                     return 
+        #si no encuentra el producto        
         print("El producto no se encuentra en el inventario.")        
     except FileNotFoundError:
         print("El archivo no existe.")
